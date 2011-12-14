5bd21642d6ada1cef67c25b60e2269ef4d866100
boxi <boxi@Linde>
2011-12-14 21:27:35 +0100

    OpticalRangeParser added

--- WeatherStation/src/ch/boxi/weatherStatistic/dto/MeasurePoint.java
+++ WeatherStation/src/ch/boxi/weatherStatistic/dto/MeasurePoint.java
@@ -4,4 +4,37 @@ public class MeasurePoint {
 	private MeasureType type;
 	private Unit unit;
 	private float amount;
+	
+	public MeasurePoint(){}
+	
+	public MeasurePoint(MeasureType type, Unit unit, float amount) {
+		this();
+		this.type = type;
+		this.unit = unit;
+		this.amount = amount;
+	}
+
+	public MeasureType getType() {
+		return type;
+	}
+
+	public void setType(MeasureType type) {
+		this.type = type;
+	}
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
 }
--- WeatherStation/src/ch/boxi/weatherStatistic/parser/metar/OpticalRangeParser.java
+++ WeatherStation/src/ch/boxi/weatherStatistic/parser/metar/OpticalRangeParser.java
@@ -1,22 +1,32 @@
 package ch.boxi.weatherStatistic.parser.metar;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ch.boxi.weatherStatistic.dto.MeasurePoint;
+import ch.boxi.weatherStatistic.dto.MeasureType;
+import ch.boxi.weatherStatistic.dto.Unit;
 
 public class OpticalRangeParser implements MetarSubParser {
 
 	@Override
 	public boolean canParse(String phrase) {
-		// TODO Auto-generated method stub
-		return false;
+		Pattern pattern = Pattern.compile("\\d{4}");
+		Matcher matcher = pattern.matcher(phrase);
+		return matcher.matches();
 	}
 
 	@Override
 	public List<MeasurePoint> parse(String phrase) throws ParseException {
-		// TODO Auto-generated method stub
-		return null;
+		List<MeasurePoint> retList = new ArrayList<>(1);
+		MeasurePoint measure = new MeasurePoint();
+		measure.setType(MeasureType.OpticalRange);
+		measure.setUnit(Unit.Meter);
+		measure.setAmount(Integer.parseInt(phrase));
+		return retList;
 	}
 
 }
